7be974c7d71e0ccaa6236ad93dd100dc797d42d2
Cesar Ivan Martinez Perez <cesar.martinez@gmstatecla.alcaldia>
2022-09-08 10:37:21 -0600

    add

--- database/seeders/RubroSeeder.php
+++ database/seeders/RubroSeeder.php
@@ -17,7 +17,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('mecanico'),
             'imagen' => ('/rubro/mecanico.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Mecanico',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -29,7 +29,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('albañil'),
             'imagen' => ('/rubro/albañil.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Albanil, Constructor',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -41,7 +41,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('fontanero'),
             'imagen' => ('/rubro/fontanero.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Fontanero',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -53,7 +53,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('medico'),
             'imagen' => ('/rubro/medico.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Medico,Doctor,Doctora',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -65,7 +65,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('niñera'),
             'imagen' => ('/rubro/niñera.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Niñera,Cuidadora,Nana',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -77,7 +77,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('artesano'),
             'imagen' => ('/rubro/artesano.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Artesano',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -89,7 +89,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('panadero'),
             'imagen' => ('/rubro/panadero.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Panadero,Panadera,Pastelera,Pastelero',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -101,7 +101,7 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('peluquero'),
             'imagen' => ('/rubro/peluquero.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Peluquero,Estilista',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
@@ -113,168 +113,169 @@ class RubroSeeder extends Seeder
             'slug' => Str::slug('musico'),
             'imagen' => ('/rubro/musico.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Musico,Cantante,Guitarrista',
             'estado' => true,
             'id_categoria' => 1,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
 
-        //Empresas
         DB::table('rubro')->insert([
-            'nombre_rubro' => 'Escritor-E',
-            'slug' => Str::slug('escritor-e'),
+            'nombre_rubro' => 'Escritor',
+            'slug' => Str::slug('escritor'),
             'imagen' => ('/rubro/escritor.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Escritor,Novelista',
             'estado' => true,
-            'id_categoria' => 2,
+            'id_categoria' => 1,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
         
         DB::table('rubro')->insert([
-            'nombre_rubro' => 'Vigilante-E',
-            'slug' => Str::slug('vigilante-e'),
+            'nombre_rubro' => 'Vigilante',
+            'slug' => Str::slug('vigilante'),
             'imagen' => ('/rubro/vigilante.png'),
             'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
+            'tags' => 'Vigilante,Cuidador',
             'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Psicólogo-E',
-            'slug' => Str::slug('psicologo-e'),
-            'imagen' => ('/rubro/psicologo.png'),
-            'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Mecánico-E',
-            'slug' => Str::slug('mecanico-e'),
-            'imagen' => ('/rubro/mecanico.png'),
-            'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
-            'estado' => true,
-            'id_categoria' => 2,
+            'id_categoria' => 1,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
         
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Albañil-E',
-            'slug' => Str::slug('albañil-e'),
-            'imagen' => ('/rubro/albañil.png'),
-            'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Fontanero-E',
-            'slug' => Str::slug('fontanero-e'),
-            'imagen' => ('/rubro/fontanero.png'),
-            'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Medico-E',
-            'slug' => Str::slug('medico-e'),
-            'imagen' => ('/rubro/medico.png'),
-            'click' => 0,
-            'tags' => 'vvvvv,hhhhh',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Niñera-E',
-            'slug' => Str::slug('niñera-e'),
-            'imagen' => ('/rubro/niñera.png'),
-            'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Artesano-E',
-            'slug' => Str::slug('artesano-e'),
-            'imagen' => ('/rubro/artesano.png'),
-            'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Panadero-E',
-            'slug' => Str::slug('panadero-e'),
-            'imagen' => ('/rubro/panadero.png'),
-            'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Peluquero-E',
-            'slug' => Str::slug('peluquero-e'),
-            'imagen' => ('/rubro/peluquero.png'),
-            'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]); 
-
-        DB::table('rubro')->insert([
-            'nombre_rubro' => 'Músico-E',
-            'slug' => Str::slug('musico-e'),
-            'imagen' => ('/rubro/musico.png'),
-            'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
-            'estado' => true,
-            'id_categoria' => 2,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
         DB::table('rubro')->insert([
             'nombre_rubro' => 'Soldador',
             'slug' => Str::slug('soldador'),
             'imagen' => ('/rubro/soldador.png'),
             'click' => 0,
-            'tags' => 'zzzzzz,yyyyyy',
+            'tags' => 'Soldador',
+            'estado' => true,
+            'id_categoria' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+
+        // Rubros de Empresas
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Chilateria',
+            'slug' => Str::slug('chilateria'),
+            'imagen' => ('/rubro/chilateria.png'),
+            'click' => 0,
+            'tags' => 'chilateria,tipicos',
             'estado' => true,
             'id_categoria' => 2,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
+        
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Laboratorio Clinico',
+            'slug' => Str::slug('laboratorio'),
+            'imagen' => ('/rubro/laboratorio-clinico.png'),
+            'click' => 0,
+            'tags' => 'laboratorio clinico,examen de sangre',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Hospitales',
+            'slug' => Str::slug('hospital'),
+            'imagen' => ('/rubro/hospital.png'),
+            'click' => 0,
+            'tags' => 'hospitales,cirugias, clinicas, Dolor de cabeza',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Mecánica Automotriz',
+            'slug' => Str::slug('taller-mecanico'),
+            'imagen' => ('/rubro/mecanica-automotriz.png'),
+            'click' => 0,
+            'tags' => 'Mecánica Automotriz,Taller Electrico,Talleres',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Farmacias',
+            'slug' => Str::slug('farmacias'),
+            'imagen' => ('/rubro/farmacia.png'),
+            'click' => 0,
+            'tags' => 'medicina,farmacias',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Artesanias',
+            'slug' => Str::slug('souvenir'),
+            'imagen' => ('/rubro/souvenir.png'),
+            'click' => 0,
+            'tags' => 'recuerdos,artesanias,souvenir',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Restaurantes',
+            'slug' => Str::slug('restaurante'),
+            'imagen' => ('/rubro/restaurante.png'),
+            'click' => 0,
+            'tags' => 'restaurante,comedor',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Salones de Belleza',
+            'slug' => Str::slug('salones'),
+            'imagen' => ('/rubro/salon-belleza.png'),
+            'click' => 0,
+            'tags' => 'salones de belleza',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Reparacion de Celulares',
+            'slug' => Str::slug('reparaciones-celulares'),
+            'imagen' => ('/rubro/raparacion-celular.png'),
+            'click' => 0,
+            'tags' => 'Reparación de celulares',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        DB::table('rubro')->insert([
+            'nombre_rubro' => 'Panaderias',
+            'slug' => Str::slug('panaderia'),
+            'imagen' => ('/rubro/pasteleria.png'),
+            'click' => 0,
+            'tags' => 'panaderias,pastelerias,pasteles',
+            'estado' => true,
+            'id_categoria' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+       
     }
 }
